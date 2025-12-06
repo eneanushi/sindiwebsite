f1f305d94b1b2918677bb4eb1ac7772dd8ca3cc3
eneanushi <eneanushi12334@gmail.com>
2025-12-05 19:21:34 -0500

    splassh cursor only on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import SplashCursor from './components/SplashCursor';
 import Home from './pages/Home';
@@ -9,20 +9,29 @@ import Resume from './pages/Resume';
 import Gallery from './pages/Gallery';
 import './App.css';
 
+function AppContent() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <div className="App">
+      {isHomePage && <SplashCursor />}
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <SplashCursor />
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/gallery" element={<Gallery />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
